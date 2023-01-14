cadd711030fae7380228569b8fcf29e6b10bee6c
Arthur Fontaine <0arthur.fontaine@gmail.com>
2023-01-14 18:17:17 +0100

    fix(back): return appartment's uuid when creating

--- back/app/Models/Appartment.php
+++ back/app/Models/Appartment.php
@@ -2,15 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Appartment extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
 
     public function users()
     {
         return $this->hasMany(User::class);
+    }
+
+    /**
+     * Get the columns that should receive a unique identifier.
+     *
+     * @return array
+     */
+    public function uniqueIds()
+    {
+        return ['uuid'];
     }
 }
