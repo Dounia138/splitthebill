f219109c85ce396f643c21117a3a0045a50356cc
thierrymgn <maignan.thierry@outlook.fr>
2023-01-15 18:22:23 +0100

    feat(front): add ticket reminder button

--- front/src/components/dashboard/Mates.tsx
+++ front/src/components/dashboard/Mates.tsx
@@ -5,6 +5,8 @@ import { AnimatePresence, motion } from 'framer-motion'
 import { UsersRepo } from '$repositories/UsersRepo'
 import { User } from '$types/api/User'
 import { UserPlusIcon } from '@heroicons/react/20/solid'
+import SendReminderForm from '$components/dashboard/SendReminderForm'
+import useUserStore from '$hooks/useUserStore'
 
 type ThreeDotsMenuProps = {
   mate: User
@@ -62,7 +64,9 @@ const Mates = () => {
   const mates = useAppartmentStore((state) => state.mates())
   const fetchAppartment = useAppartmentStore((state) => state.fetch)
   const inviteLink = `${location.origin}/inscription/?inviteCode=${appartment?.uuid}`
+  const me = useUserStore((state) => state.user)
   const [copyButtonText, setCopyButtonText] = useState('Copier le lien')
+  const [userId, setUserId] = useState<number | null>(null)
 
   const copyInviteLink = () => {
     navigator.clipboard.writeText(inviteLink)
@@ -72,104 +76,130 @@ const Mates = () => {
     }, 2000)
   }
 
+  const closeSendReminderForm = () => {
+    setUserId(null)
+  }
+
   useEffect(() => {
     fetchAppartment()
   }, [])
 
   return (
-    <ul
-      role="list"
-      className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
-    >
-      {mates.map((mate) => (
+    <>
+      <ul
+        role="list"
+        className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
+      >
+        {mates.map((mate) => (
+          <li
+            key={mate.email}
+            className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-white text-center shadow"
+          >
+            <div className="flex flex-1 flex-col p-8 relative">
+              <ThreeDotsMenu mate={mate} />
+              <img
+                className="mx-auto h-32 w-32 flex-shrink-0 rounded-full"
+                src={mate.avatarUrl}
+                alt=""
+              />
+              <h3 className="mt-6 text-sm font-medium text-gray-900">
+                {mate.name}
+              </h3>
+              <dl className="mt-1 flex flex-grow flex-col justify-between">
+                <dt className="sr-only">Title</dt>
+                <dd className="text-sm text-gray-500">{mate.email}</dd>
+                <dt className="sr-only">Role</dt>
+                <dd className="mt-3">
+                  <span
+                    className={`${mate.isAdmin
+                      ? 'bg-orange-100 text-orange-800'
+                      : 'bg-indigo-100 text-indigo-800'
+                      } rounded-full px-2 py-1 text-xs font-medium`}
+                  >
+                    {mate.isAdmin ? 'Admin' : 'Membre'}
+                  </span>
+                </dd>
+              </dl>
+            </div>
+            <div>
+              <div className="-mt-px flex divide-x divide-gray-200">
+                {mate.id !== me?.id && (
+                  <div className="flex w-0 flex-1">
+                    <a
+                      onClick={() => setUserId(mate.id)}
+                      className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center rounded-bl-lg border border-transparent py-4 text-sm font-medium text-gray-700 hover:text-gray-500"
+                    >
+                      Rappeler un ticket
+                    </a>
+                  </div>
+                )}
+                {mate.id !== me?.id && (
+                  <div className="-ml-px flex w-0 flex-1">
+                    <a
+                      // href={`tel:${mate.phoneNumber}`}
+                      className="relative inline-flex w-0 flex-1 items-center justify-center rounded-br-lg border border-transparent py-4 text-sm font-medium text-gray-700 hover:text-gray-500"
+                    >
+                      Virer de la colloc'
+                    </a>
+                  </div>
+                )}
+                {mate.id === me?.id && (
+                  <div className="-ml-px flex w-0 flex-1">
+                    <a
+                      // href={`tel:${mate.phoneNumber}`}
+                      className="relative inline-flex w-0 flex-1 items-center justify-center rounded-br-lg border border-transparent py-4 text-sm font-medium text-gray-700 hover:text-gray-500"
+                    >
+                      Quitter la colloc'
+                    </a>
+                  </div>
+                )}
+              </div>
+            </div>
+          </li>
+        ))}
         <li
-          key={mate.email}
           className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-white text-center shadow"
         >
-          <div className="flex flex-1 flex-col p-8 relative">
-            <ThreeDotsMenu mate={mate} />
-            <img
-              className="mx-auto h-32 w-32 flex-shrink-0 rounded-full"
-              src={mate.avatarUrl}
-              alt=""
+          <div className="flex justify-center items-center flex-1 flex-col p-8 relative">
+            <UserPlusIcon
+              className="h-24 w-24 text-indigo-600"
+              aria-hidden="true"
             />
-            <h3 className="mt-6 text-sm font-medium text-gray-900">
-              {mate.name}
-            </h3>
-            <dl className="mt-1 flex flex-grow flex-col justify-between">
-              <dt className="sr-only">Title</dt>
-              <dd className="text-sm text-gray-500">{mate.email}</dd>
-              <dt className="sr-only">Role</dt>
-              <dd className="mt-3">
-                <span
-                  className={`${mate.isAdmin
-                    ? 'bg-orange-100 text-orange-800'
-                    : 'bg-indigo-100 text-indigo-800'
-                    } rounded-full px-2 py-1 text-xs font-medium`}
-                >
-                  {mate.isAdmin ? 'Admin' : 'Membre'}
-                </span>
-              </dd>
-            </dl>
+            <div className="mt-6 mb-3">
+              <p className="text-sm text-gray-500">Invitez votre nouveau colloc en lui envoyant ce lien !</p>
+            </div>
+            <input
+              id="invitation"
+              name="invitation"
+              type="text"
+              autoComplete="invitation"
+              readOnly
+              value={inviteLink}
+              onClick={(e) => { copyInviteLink(); e.currentTarget.select() }}
+              className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+            />
           </div>
           <div>
             <div className="-mt-px flex divide-x divide-gray-200">
-              <div className="flex w-0 flex-1">
-                <a
-                  href={`mailto:${mate.email}`}
-                  className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center rounded-bl-lg border border-transparent py-4 text-sm font-medium text-gray-700 hover:text-gray-500"
-                >
-                  Rappeler un ticket
-                </a>
-              </div>
               <div className="-ml-px flex w-0 flex-1">
-                <a
-                  href={`tel:${mate.phoneNumber}`}
-                  className="relative inline-flex w-0 flex-1 items-center justify-center rounded-br-lg border border-transparent py-4 text-sm font-medium text-gray-700 hover:text-gray-500"
+                <p
+                  className="relative cursor-pointer inline-flex w-0 flex-1 items-center justify-center rounded-br-lg border border-transparent py-4 text-sm font-medium text-gray-700 hover:text-gray-500"
+                  onClick={copyInviteLink}
                 >
-                  Virer de la colloc'
-                </a>
+                  {copyButtonText}
+                </p>
               </div>
             </div>
           </div>
         </li>
-      ))}
-      <li
-        className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-white text-center shadow"
-      >
-        <div className="flex justify-center items-center flex-1 flex-col p-8 relative">
-          <UserPlusIcon
-            className="h-24 w-24 text-indigo-600"
-            aria-hidden="true"
-          />
-          <div className="mt-6 mb-3">
-            <p className="text-sm text-gray-500">Invitez votre nouveau colloc en lui envoyant ce lien !</p>
-          </div>
-          <input
-            id="invitation"
-            name="invitation"
-            type="text"
-            autoComplete="invitation"
-            readOnly
-            value={inviteLink}
-            onClick={(e) => { copyInviteLink(); e.currentTarget.select() }}
-            className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-          />
-        </div>
-        <div>
-          <div className="-mt-px flex divide-x divide-gray-200">
-            <div className="-ml-px flex w-0 flex-1">
-              <p
-                className="relative cursor-pointer inline-flex w-0 flex-1 items-center justify-center rounded-br-lg border border-transparent py-4 text-sm font-medium text-gray-700 hover:text-gray-500"
-                onClick={copyInviteLink}
-              >
-                {copyButtonText}
-              </p>
-            </div>
-          </div>
-        </div>
-      </li>
-    </ul>
+      </ul>
+      {userId && (
+        <SendReminderForm
+          handleClose={closeSendReminderForm}
+          userId={userId}
+        />
+      )}
+    </>
   )
 }
 
---
+++ front/src/components/dashboard/SendReminderForm.tsx
@@ -0,0 +1,152 @@
+import useAppartmentStore from "$hooks/useAppartmentStore"
+import useUserStore from "$hooks/useUserStore"
+import { CalendarIcon } from '@heroicons/react/20/solid'
+import { CurrencyEuroIcon } from '@heroicons/react/24/outline'
+import { UsersRepo } from '$repositories/UsersRepo'
+import { useState, useEffect } from 'react'
+import { User } from '$types/api/User'
+
+function SendReminderForm({ userId, handleClose }: { userId: number, handleClose: () => void }) {
+    const tickets = useAppartmentStore((state) => state.tickets())
+    const payments = useAppartmentStore((state) => state.payments())
+    const owesPayments = useAppartmentStore((state) => state.owesPayments())
+    const residents = useAppartmentStore((state) => state.mates())
+
+    const [user, setUser] = useState<User | null>(null)
+    useEffect(() => {
+        UsersRepo.get(userId).then(({ user }) => setUser(user))
+    }, [userId])
+
+    const me = useUserStore((state) => state.user)
+    const filteredTickets = tickets.reduce((acc, ticket) => {
+        const foundOwesPayment = owesPayments.filter((owesPayment) => owesPayment.forTicketId === ticket.id && owesPayment.payerId === userId)
+        const foundPayments = payments.filter((payment) => foundOwesPayment.some((owesPayment) => owesPayment.id === payment.forOwesPaymentId))
+        const paymentsSum = foundPayments.reduce((acc, payment) => acc + payment.amount, 0)
+
+        if (ticket.id in acc) {
+            acc[ticket.id].remainingAmount -= paymentsSum
+            if (acc[ticket.id].remainingAmount <= 0) {
+                delete acc[ticket.id]
+            }
+        }
+
+        return acc
+    }, Object.fromEntries(tickets.map((ticket) => [ticket.id, { ticket, remainingAmount: ticket.amount }])))
+
+    return (
+        <>
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+            <div className="fixed inset-x-1/3 inset-y-1/4 h-96 max-h-full my-8 z-10 overflow-y-auto bg-white rounded-md ">
+                <div className="space-y-8 divide-y divide-gray-200 m-10">
+                    <div className="space-y-8 divide-y divide-gray-200 sm:space-y-5">
+                        <div className="space-y-6 sm:space-y-5">
+                            <div>
+                                <h3 className="text-lg font-medium leading-6 text-gray-900">
+                                    Création du Rappel
+                                </h3>
+                            </div>
+                            <div className="space-y-6 sm:space-y-5">
+
+                                <ul role="list" className="divide-y divide-gray-200">
+
+                                    {Object.entries(filteredTickets).map(([ticketId, { ticket, remainingAmount }]) => (
+                                        <li key={ticketId}>
+                                            <a
+                                                href={`mailto:${user?.email}?subject=Rappel de paiement pour ${ticket.name}&body=Bonjour ${user?.name},%0D%0A%0D%0AJe te rappelle que tu dois encore payer ${remainingAmount}€ pour ${ticket.name}.%0D%0A%0D%0ACordialement,%0D%0A%0D%0A${me?.name}`}
+                                                className="block hover:bg-gray-50"
+                                            >
+                                                <div className="px-4 py-4 sm:px-6">
+                                                    <div className="flex items-center justify-between">
+                                                        <p className="truncate text-sm font-medium text-indigo-600">
+                                                            {ticket.name}
+                                                        </p>
+                                                        <div className="ml-2 flex flex-shrink-0">
+                                                            <p className="inline-flex rounded-full bg-red-100 px-2 text-xs font-semibold leading-5 text-red-800">
+                                                                {ticket.amount}€
+                                                            </p>
+                                                        </div>
+                                                    </div>
+                                                    <div className="mt-2 sm:flex sm:justify-between">
+                                                        <div className="sm:flex">
+                                                            <p className="flex items-center text-sm text-gray-500">
+                                                                par{' '}
+                                                                {
+                                                                    residents.find(
+                                                                        (resident) => resident.id === ticket.creatorId,
+                                                                    )?.name
+                                                                }
+                                                            </p>
+                                                        </div>
+                                                        <div className="flex gap-4">
+                                                            {ticket.expirationDate && (
+                                                                <div className="mt-2 flex items-center text-sm text-gray-500 sm:mt-0">
+                                                                    <CalendarIcon
+                                                                        className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
+                                                                        aria-hidden="true"
+                                                                    />
+                                                                    <p>
+                                                                        A régler avant le {/* TODO: format date */}
+                                                                        <time
+                                                                            dateTime={ticket.expirationDate.toDateString()}
+                                                                        >
+                                                                            {ticket.expirationDate.toLocaleDateString('fr')}
+                                                                        </time>
+                                                                    </p>
+                                                                </div>
+                                                            )}
+                                                            <div className="mt-2 flex items-center text-sm text-gray-500 sm:mt-0">
+                                                                <CurrencyEuroIcon
+                                                                    className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
+                                                                    aria-hidden="true"
+                                                                />
+                                                                <p>
+                                                                    Reste{' '}
+                                                                    {owesPayments.reduce(
+                                                                        (acc, owesPayment) =>
+                                                                            acc +
+                                                                            (owesPayment.forTicketId === ticket.id
+                                                                                ? owesPayment.requestedAmount
+                                                                                : 0),
+                                                                        0,
+                                                                    ) -
+                                                                        payments.reduce(
+                                                                            (acc, payment) =>
+                                                                                acc +
+                                                                                (owesPayments.find(
+                                                                                    (owesPayment) =>
+                                                                                        owesPayment.id === payment.forOwesPaymentId,
+                                                                                )?.forTicketId === ticket.id
+                                                                                    ? payment.amount
+                                                                                    : 0),
+                                                                            0,
+                                                                        )}
+                                                                    €{' '}
+                                                                </p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        </li>
+                                    ))}
+                                </ul>
+
+                                <div className="sm:border-t sm:border-gray-200 sm:pt-5 flex justify-end">
+                                    <button
+                                        type="button"
+                                        className="rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                                        onClick={handleClose}
+                                    >
+                                        Quitter
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default SendReminderForm
