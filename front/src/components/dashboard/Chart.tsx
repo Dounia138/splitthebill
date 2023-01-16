7967ff49babeb4844cc58e3d027986bed1d6fc3b
Arthur Fontaine <0arthur.fontaine@gmail.com>
2023-01-16 02:36:00 +0100

    feat(front): sync chart with api data

--- front/src/components/dashboard/Chart.tsx
+++ front/src/components/dashboard/Chart.tsx
@@ -1,3 +1,4 @@
+import { useAppartmentStore } from '$hooks/useAppartmentStore'
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -20,24 +21,6 @@ ChartJS.register(
   Legend,
 )
 
-const data = {
-  labels: new Array(6)
-    .fill(null)
-    .map(
-      (_, i) =>
-        new Date(new Date().setMonth(new Date().getMonth() + 7 + i))
-          .toLocaleString('fr-FR', { month: 'long' })
-          .split(', ')[0],
-    ), // get the last 6 months
-  datasets: [
-    {
-      label: "Dépenses de la colloc'",
-      data: [1000, 1100, 1050, 1030, 1020, 1010],
-      borderColor: 'rgb(88 80 236)',
-    },
-  ],
-}
-
 const options = {
   elements: {
     line: {
@@ -52,8 +35,33 @@ const options = {
 }
 
 const Chart = () => {
+  const tickets = useAppartmentStore((state) => state.tickets())
+
+  const MONTH_TO_MILLISECONDS = 1000 * 60 * 60 * 24 * 30
+
+  const lastSixMonths = new Array(6)
+    .fill(null)
+    .map((_, i) => new Date(Date.now() - MONTH_TO_MILLISECONDS * i))
+    .reverse()
+
+  const data = {
+    labels: lastSixMonths.map((date) => date.toLocaleString('fr-FR', { month: 'long' }).split(', ')[0]),
+    datasets: [
+      {
+        label: "Dépenses de la colloc'",
+        data: lastSixMonths.map((date) =>
+          tickets.filter((ticket) => {
+            const ticketDate = new Date(ticket.createdAt)
+            return ticketDate.getMonth() === date.getMonth() && ticketDate.getFullYear() === date.getFullYear()
+          }).reduce((acc, ticket) => acc + ticket.amount, 0),
+        ),
+        borderColor: 'rgb(88 80 236)',
+      },
+    ],
+  }
+
   return (
-    <div className="overflow-hidden bg-white shadow sm:rounded-md">
+    <div className="overflow-hidden bg-white shadow sm:rounded-md h-max">
       <div className="border-b border-gray-200 bg-white px-4 py-5 sm:px-6">
         <div className="-ml-4 -mt-2 flex flex-wrap h-10 items-center justify-between sm:flex-nowrap">
           <div className="ml-4 mt-2">
